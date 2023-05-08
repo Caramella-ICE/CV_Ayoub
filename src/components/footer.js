7e45492d9770c585f27f0bd334f09ed8566c3ef7
AyoubAzzz <ayoublines@gmail.com>
2023-05-08 14:33:04 +0200

    Update: Footer& Header

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,9 @@ import React from "react";
 
 
 function Footer() {
-    return ( <p>footer</p> );
+    return ( <div className="footer">
+        <p>"22 years old with an engineer Degree ( M2 ) in computer science and information systems at the University of Technology of Troyes (UTT), I am looking for a full time job as a software engineer starting 20th September 2023."</p>
+        </div> );
 }
 
 export default Footer;
--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,34 @@
 import React from "react"
 
 function Header(){
-    return (<nav aria-label="breadcrumb">
-                <ol className="breadcrumb">
-                    <li className="breadcrumb-item active" aria-current="page">SEND ME A MESSAGE </li>
-                </ol>
-            </nav>)
-}
+    return (<div className="header">
+             
+            
+            <div>
+                <img src="https://cdn-icons-png.flaticon.com/512/64/64665.png"/>
+                <p>Paris/France</p>
+            </div>
+            <div>
+                <img src="https://www.freeiconspng.com/thumbs/phone-icon/phone-icon-clip-art--royalty--7.png"/>
+                <p>+33749341664</p>
+            </div>
+            <div onClick={()=>window.open("https://mail.google.com/mail/u/0/#inbox?compose=CllgCJNqtbmHqMbQZksqDbSpHLtwkNdDnVfqcrWDXVCmZBvQwLTXCjzQRVZKHQSwsKszgGQGZkg")} style={{cursor:"pointer"}}> 
+                <img  src="https://cdn-icons-png.flaticon.com/512/95/95627.png"/>
+                <p>Ayoublines@gmail.com</p>
+            </div>
+            <div onClick={()=>window.open("https://www.linkedin.com/in/ayoub-azzouzi-3275031b0/")} style={{cursor:"pointer"}}>
+                <img src="https://www.edigitalagency.com.au/wp-content/uploads/new-linkedin-logo-white-black-png.png"/>
+                
+            </div>
+            <div onClick={()=>window.open("https://github.com/Caramella-ICE")} style={{cursor:"pointer"}}>
+                <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png"/>
+
+            </div>
+
+            
+            
+            </div>
+            )
+}       
 
 export default Header
